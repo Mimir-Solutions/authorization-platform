2245460d4124bd3f17f7905164639ccb179ad19b
cyotee doge <cyotee@gmail.com>
2020-11-27 00:16:05 -0800

    Renamed Platform to Protocol added Client

---
+++ contracts/AuthorizationProtocolClient.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.4;
+
+import "hardhat/console.sol";
+
+abstract contract AuthorizationProtocolClient {
+
+    constructor() {}
+
+    function _registerContract() internal {}
+
+    function _registerRoles() internal {}
+}
